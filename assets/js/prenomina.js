9daf84f360d343198f7157bea831fd67846ddff1
nchavezs <n.chavezsantoyo@ugto.mx>
2022-01-19 00:36:45 -0600

    fix

--- assets/js/prenomina.js
+++ assets/js/prenomina.js
@@ -148,7 +148,6 @@ function prenomina() {
             periodo: $("#periodo").val()
         },
         success: function (data) {
-            alert(data);
             if (data != 0) {
                 Swal.fire({
                     title: 'Correcto',
