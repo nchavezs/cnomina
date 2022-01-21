7a48312fb12ee07c5951cd79f790ff970e8c3a9b
nchavezs <n.chavezsantoyo@ugto.mx>
2022-01-21 04:43:29 -0600

    version

--- catalogos.php
+++ catalogos.php
@@ -19,12 +19,12 @@ if (rol() != 1) {
 	</title>
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link href="assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
-	<link href="assets/css/select2.css?v=3.2.0" rel="stylesheet" />
+	<link href="assets/css/material-dashboard.css?v=3.2.1" rel="stylesheet" />
+	<link href="assets/css/select2.css?v=3.2.1" rel="stylesheet" />
 	<link href="assets/js/plugins/datatables/datatables.min.css" rel="stylesheet" />
 	<link href="assets/css/animate.css" rel="stylesheet" />
 	<link href="assets/css/datepicker.min.css" rel="stylesheet" />
-	<link href="assets/css/sweetalert2.min.css?v=3.2.0" rel="stylesheet" />
+	<link href="assets/css/sweetalert2.min.css?v=3.2.1" rel="stylesheet" />
 	<link rel="stylesheet" href="assets/js/plugins/animate/adp.css">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
 
@@ -240,15 +240,15 @@ if ($varFoto != null) {
 	<script src="assets/js/plugins/sweetalert2.min.js"></script>
 	<script src="assets/js/plugins/jquery.dataTables.min.js"></script>
 	<script src="assets/js/plugins/bootstrap-notify.js"></script>
-	<script src="assets/js/material-dashboard.js?v=3.2.0" type="text/javascript"></script>
+	<script src="assets/js/material-dashboard.js?v=3.2.1" type="text/javascript"></script>
 	<script src="assets/js/plugins/datatables/datatables.min.js"></script>
 	<script src="assets/js/datepicker.min.js"></script>
 	<script src="assets/js/plugins/datepicker.es.js"></script>
 	<script src="assets/js/plugins/animate/adp.js"></script>
 	<script src="assets/js/block.js"></script>
-	<script src="assets/js/catalagos.js?v=3.2.0"></script>
-	<script src="assets/js/sesion.js?v=3.2.0"></script>
-	<script src="assets/js/mensajes.js?v=3.2.0"></script>
+	<script src="assets/js/catalagos.js?v=3.2.1"></script>
+	<script src="assets/js/sesion.js?v=3.2.1"></script>
+	<script src="assets/js/mensajes.js?v=3.2.1"></script>
 
 </body>
 
