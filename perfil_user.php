60c00a5c534c5cb24695692db890b7ec9d43b3bc
nchavezs <n.chavezsantoyo@ugto.mx>
2022-02-14 23:23:10 -0600

    ver

--- perfil_user.php
+++ perfil_user.php
@@ -37,11 +37,11 @@ if ($conexion->query($sql)) {
     </title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="assets/css/material-dashboard.css?v=3.3.3" rel="stylesheet" />
+    <link href="assets/css/material-dashboard.css?v=3.3.4" rel="stylesheet" />
     <link href="assets/css/animate.css" rel="stylesheet" />
-    <link href="assets/css/select2.css?v=3.3.3" rel="stylesheet" />
+    <link href="assets/css/select2.css?v=3.3.4" rel="stylesheet" />
     <link href="assets/css/datepicker.min.css" rel="stylesheet" />
-    <link href="assets/css/sweetalert2.min.css?v=3.3.3" rel="stylesheet" />
+    <link href="assets/css/sweetalert2.min.css?v=3.3.4" rel="stylesheet" />
     <link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
 </head>
 
@@ -315,12 +315,12 @@ if ($conexion->query($sql)) {
     <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
     <script src="assets/js/plugins/sweetalert2.min.js"></script>
     <script src="assets/js/plugins/bootstrap-notify.js"></script>
-    <script src="assets/js/material-dashboard.js?v=3.3.3" type="text/javascript"></script>
+    <script src="assets/js/material-dashboard.js?v=3.3.4" type="text/javascript"></script>
     <script src="assets/js/plugins/jquery.dataTables.min.js"></script>
-    <script src="assets/js/sesion.js?v=3.3.3"></script>
+    <script src="assets/js/sesion.js?v=3.3.4"></script>
     <script src="assets/js/block.js"></script>
-    <script src="assets/js/perfil.js?v=3.3.3"></script>
-    <script src="assets/js/mensajes-user.js?v=3.3.3"></script>
+    <script src="assets/js/perfil.js?v=3.3.4"></script>
+    <script src="assets/js/mensajes-user.js?v=3.3.4"></script>
     <script src="assets/js/select.js"></script>
 
 </body>
