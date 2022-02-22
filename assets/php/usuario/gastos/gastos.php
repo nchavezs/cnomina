9c2329f02c4bbdf318e6a09c786c55e50a223037
nchavezs <n.chavezsantoyo@ugto.mx>
2022-02-22 15:13:04 -0600

    fix user anterior

--- assets/php/usuario/gastos/gastos.php
+++ assets/php/usuario/gastos/gastos.php
@@ -4,12 +4,38 @@ include "../../conexion.php";
 $conexion = conexion();
 $id = $_SESSION["usuario"];
 
-$sql = "SELECT * FROM Usuario WHERE RFC = '".$id."'";
-$consulta = mysqli_query($conexion, $sql);
-$usuario = mysqli_fetch_array($consulta);
-
 $sql = "SELECT * FROM Gastos WHERE RFC = '" . $id."'";
-$consulta = $conexion->query($sql);
+$consulta = mysqli_query($conexion, $sql);
+
+// $ano = date("Y");
+// $ano1 = 2018;
+// $ano2 = 2019;
+// $ano3 = 2020;
+// $ano4 = 2021;
+
+// if ($ano1 == $ano) {
+//     $ano1 = 'selected="true"';
+// } else {
+//     $ano1 = '';
+// }
+
+// if ($ano2 == $ano) {
+//     $ano2 = 'selected="true"';
+// } else {
+//     $ano2 = '';
+// }
+
+// if ($ano3 == $ano) {
+//     $ano3 = 'selected="true"';
+// } else {
+//     $ano3 = '';
+// }
+
+// if ($ano4 == $ano) {
+//     $ano4 = 'selected="true"';
+// } else {
+//     $ano4 = '';
+// }
 
 $mes = date("m");
 $mes1 = 1;
@@ -26,89 +52,90 @@ $mes11 = 11;
 $mes12 = 12;
 
 if ($mes1 == $mes) {
-    $mes1 = 'selected"';
+    $mes1 = 'selected="true"';
 } else {
     $mes1 = '';
 }
 
 if ($mes2 == $mes) {
-    $mes2 = 'selected"';
+    $mes2 = 'selected="true"';
 } else {
     $mes2 = '';
 }
 
 if ($mes3 == $mes) {
-    $mes3 = 'selected"';
+    $mes3 = 'selected="true"';
 } else {
     $mes3 = '';
 }
 
 if ($mes4 == $mes) {
-    $mes4 = 'selected"';
+    $mes4 = 'selected="true"';
 } else {
     $mes4 = '';
 }
 
 if ($mes5 == $mes) {
-    $mes5 = 'selected"';
+    $mes5 = 'selected="true"';
 } else {
     $mes5 = '';
 }
 
 if ($mes6 == $mes) {
-    $mes6 = 'selected"';
+    $mes6 = 'selected="true"';
 } else {
     $mes6 = '';
 }
 
 if ($mes7 == $mes) {
-    $mes7 = 'selected"';
+    $mes7 = 'selected="true"';
 } else {
     $mes7 = '';
 }
 
 if ($mes8 == $mes) {
-    $mes8 = 'selected"';
+    $mes8 = 'selected="true"';
 } else {
     $mes8 = '';
 }
 
 if ($mes9 == $mes) {
-    $mes9 = 'selected"';
+    $mes9 = 'selected="true"';
 } else {
     $mes9 = '';
 }
 
 if ($mes10 == $mes) {
-    $mes10 = 'selected"';
+    $mes10 = 'selected="true"';
 } else {
     $mes10 = '';
 }
 
 if ($mes11 == $mes) {
-    $mes11 = 'selected"';
+    $mes11 = 'selected="true"';
 } else {
     $mes11 = '';
 }
 
 if ($mes12 == $mes) {
-    $mes12 = 'selected"';
+    $mes12 = 'selected="true"';
 } else {
     $mes12 = '';
 }
 
-echo '<div class="ver_opciones">
-        <select id="ano" class="sources">';
+echo '<div class="container-fluid">
+        <select name="sources" id="ano" class="custom-select sources">';
         $ano = date("Y");
         for($i=2022;$i<=2025;$i++){
-            $select_ano = "";
             if($ano == $i)
                 $select_ano = "selected";
+            else
+                $select_ano = "";
             echo '<option '.$select_ano.' value="'.$i.'">'.$i.'</option>';
         }
         echo '</select>
 
-		<select id="mes" class="sources">
+		<select id="mes" class="custom-select sources">
 			<option ' . $mes1 . ' value="1">Enero</option>
 			<option ' . $mes2 . ' value="2">Febrero</option>
 			<option ' . $mes3 . ' value="3">Marzo</option>
@@ -123,7 +150,11 @@ echo '<div class="ver_opciones">
 			<option ' . $mes12 . ' value="12">Diciembre</option>
 		</select>
 	</div>
-	<div class="card">
+	<div class="card card-profile ">
+		<div class="card-header card-header-primary">
+			<h4 class="card-title" id="titulo-gastos">GASTOS MÉDICOS</h4>
+			<p class="card-category" id="detalle-gastos"></p>
+		</div>
 		<div class="card-body">
 			<div class="caja-gastos"></div>
 		</div>
--- assets/php/usuario/permisos/permisos.php
+++ assets/php/usuario/permisos/permisos.php
@@ -1,15 +1,51 @@
 <?php
-echo '<div class="card">
-		<div class="card-body">
-			<div id="caja-permiso"></div>
-		</div>
-	</div>
-	<select id="ano" class="sources">';
-		$ano = date("Y");
-		for ($i = 2022; $i <= 2025; $i++) { if ($ano==$i) { $select_ano="selected" ; } else { $select_ano="" ; }
-			echo '<option ' . $select_ano . ' value="' . $i . '">' . $i . '</option>' ; } echo '</select>
-					<select id="permiso" class="sources">
+	// $ano = date("Y");
+	// $ano1 = 2018;
+	// $ano2 = 2019;
+	// $ano3 = 2020;
+	// $ano4 = 2021;
+
+	// if($ano1 == $ano)
+	// 	$ano1 = 'selected="true"';
+	// else
+	// 	$ano1 = '';
+	// if($ano2 == $ano)
+	// 	$ano2 = 'selected="true"';
+	// else
+	// 	$ano2 = '';
+	// if($ano3 == $ano)
+	// 	$ano3 = 'selected="true"';
+	// else
+	// 	$ano3 = '';
+	// if($ano4 == $ano)
+	// 	$ano4 = 'selected="true"';
+	// else
+	// 	$ano4 = '';
+	 
+
+	echo'<div class="container-fluid">
+					<select name="sources" id="ano" class="custom-select sources">';
+					$ano = date("Y");
+					for($i=2022;$i<=2025;$i++){
+						if($ano == $i)
+							$select_ano = "selected";
+						else
+							$select_ano = "";
+						echo '<option '.$select_ano.' value="'.$i.'">'.$i.'</option>';
+					}
+					echo '</select>
+					<select name="sources" id="permiso" class="custom-select sources">
 						<option value="0" selected="true">Permiso con goce de sueldo</option>
 						<option value="1">Permiso sin goce de sueldo</option>
 					</select>
-	';
+			</div>
+			<div class="card card-profile ">
+				<div class="card-header card-header-primary">
+					<h4 class="card-title ">PERMISOS ECONÓMICOS</h4>
+					<p id="total" class="card-category">Total de permisos: 0</p>
+				</div>
+				<div class="card-body">
+					<div id="caja-permiso"></div>
+				</div>
+			</div>';
+?>
