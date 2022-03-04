e624eab1444d6d67f743239318b40c1342832c15
nchavezs <n.chavezsantoyo@ugto.mx>
2022-03-04 13:52:58 -0600

    eliminar empleado

--- assets/php/eliminar_empleado.php
+++ assets/php/eliminar_empleado.php
@@ -9,36 +9,31 @@ if ($rol != 1) {
     $conexion = conexion();
     $id = $_POST['id'];
 
-    $sql = "DELETE FROM Usuario WHERE RFC = '" . $id . "' AND categoria = 'user'";
-    if ($conexion->query($sql)) {
-        // $sql = "DELETE FROM Movimiento WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Baja WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Descuento WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Permiso WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Vacacion WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Beneficiario WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Mensaje WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Archivo WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Pase WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        // $sql = "DELETE FROM Reingreso WHERE RFC = '" . $id . "'";
-        // $conexion->query($sql);
-        $sql = "UPDATE Plaza SET RFC = NULL WHERE RFC = '" . $id . "'";
-        $conexion->query($sql);
-        $sql = "DELETE FROM Historial_Plaza WHERE RFC = '" . $id . "'";
-        $conexion->query($sql);
-        $sql = "DELETE FROM Empleado WHERE RFC = '" . $id . "'";
-        $conexion->query($sql);
+    mysqli_autocommit($conexion, false);
+    $errors = [];
+
+
+    $sql1 = "DELETE FROM Usuario WHERE RFC = '" . $id . "' AND categoria = 'user'";
+
+    $sql2 = "DELETE FROM Empleado WHERE RFC = '" . $id . "'";
+
+    $sql3 = "UPDATE Plaza SET RFC = NULL WHERE RFC = '" . $id . "'";
+
+    if (!$conexion->query($sql1)) {
+        $errors[] = $conexion->error;
+    }
+    if (!$conexion->query($sql2)) {
+        $errors[] = $conexion->error;
+    }
+    if (!$conexion->query($sql3)) {
+        $errors[] = $conexion->error;
+    }
+
+    if (count($errors) === 0) {
+        $conexion->commit();
         echo 1;
     } else {
+        $conexion->rollback();
         echo 0;
     }
 
