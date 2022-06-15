120a415973d1005cdb5cd760db5a00ec9a2b62a8
Nicolás Chávez Santoyo <n.chavezsantoyo@gmail.com>
2022-06-15 17:07:41 -0500

    rol fix

--- assets/php/agregar_rol.php
+++ assets/php/agregar_rol.php
@@ -2,27 +2,31 @@
 include "conexion.php";
 $conexion = conexion();
 $nombre = trim(mb_strtoupper($_POST['nombre']));
-$autorizaciones = $_POST['autorizaciones'];
+$autorizaciones = $_POST['autorizaciones'] ?? [];
 
-$sql = "SELECT * FROM Rol WHERE nombre = '" . $nombre . "'";
-$consulta = $conexion->query($sql);
-if ($consulta && (mysqli_num_rows($consulta) == 0)) {
-    $sql = "INSERT INTO Rol(nombre) VALUES('" . $nombre . "')";
-    if ($conexion->query($sql)) {
-        $rol = mysqli_insert_id($conexion);
-
-        foreach($autorizaciones as $autorizacion){
-            $sql = "INSERT INTO Rol_Autorizacion(id_autorizacion, id_rol) VALUES(".$autorizacion.", ".$rol.")";
-            $conexion->query($sql);
+if(sizeof($autorizaciones) > 0){
+    $sql = "SELECT * FROM Rol WHERE nombre = '" . $nombre . "'";
+    $consulta = $conexion->query($sql);
+    if ($consulta && (mysqli_num_rows($consulta) == 0)) {
+        $sql = "INSERT INTO Rol(nombre) VALUES('" . $nombre . "')";
+        if ($conexion->query($sql)) {
+            $rol = mysqli_insert_id($conexion);
+    
+            foreach($autorizaciones as $autorizacion){
+                $sql = "INSERT INTO Rol_Autorizacion(id_autorizacion, id_rol) VALUES(".$autorizacion.", ".$rol.")";
+                $conexion->query($sql);
+            }
+    
+    
+            echo 1;
+        } else {
+            echo "Error al registrar el rol.";
         }
-
-
-        echo 1;
     } else {
-        echo "Error al registrar el rol.";
+        echo "Ya existe un rol con este nombre.";
     }
-} else {
-    echo "Ya existe un rol con este nombre.";
+    
+    $conexion->close();
+}else{
+    echo "Asigna al menos un permiso";
 }
-
-$conexion->close();
