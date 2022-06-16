8a6ec11ab36e5340213139d822980e2b10ba0bd1
Nicolás Chávez Santoyo <n.chavezsantoyo@gmail.com>
2022-06-16 02:42:00 -0500

    eliminar usuario fix

--- assets/php/eliminar_usuario.php
+++ assets/php/eliminar_usuario.php
@@ -5,6 +5,11 @@ $id = $_POST['id'];
 
 $sql = "DELETE FROM Usuario WHERE RFC = '".$id."'";
 if($conexion->query($sql)){
+    $sql = "DELETE FROM Autorizacion_Usuario WHERE RFC = '".$id."'";
+    $conexion->query($sql);
+    $sql = "DELETE FROM Rol_Usuario WHERE RFC = '".$id."'";
+    $conexion->query($sql);
+    
     echo 1;
 }else{
     echo "Error al eliminar usuario.";
