629b31893a0622d3aa9e2548d885f32d1f562a6a
nchavezs <n.chavezsantoyo@ugto.mx>
2023-01-19 20:26:38 -0600

    presupuesto

--- assets/php/importar_presupuesto.php
+++ assets/php/importar_presupuesto.php
@@ -66,6 +66,7 @@ function eliminar_simbolos($string)
 $total_departamentos = 0;
 $total_puestos = 0;
 $total_plazas = 0;
+$limite = "";
 $errores = [];
 
 $archivo = $_FILES['file']['tmp_name'];
@@ -133,25 +134,36 @@ if ($datos[0] != "PUESTO" && $datos[1] != "DEPARTAMENTO" && $datos[2] != "DIAS P
             // --------------------------------------------------------------------------------------------------
             $sql = "SELECT * FROM Plaza WHERE RFC IS NOT NULL AND id_puesto = " . $id_puesto;
             $consulta = $conexion->query($sql);
+            $plazas_usadas = 0;
             if ($consulta) {
                 while ($plaza = mysqli_fetch_array($consulta)) {
-                    $sql = "INSERT INTO Plaza(id_puesto, dias, RFC) VALUES(" . $id_puesto . ", 365, '" . $plaza["RFC"] . "')";
-                    $conexion->query($sql);
+                    if(++$plazas_usadas <= $cantidad){
+                        $sql = "UPDATE Plaza SET RFC = NULL WHERE id_plaza = " . $plaza["id_plaza"];
+                        $conexion->query($sql);
+    
+                        $ano = date("Y", strtotime($plaza["elaboracion"]));
+                        $sql = "SELECT id_historial_plaza FROM Historial_Plaza WHERE RFC = '" . $plaza["RFC"] . "' ORDER BY elaboracion desc LIMIT 1";
+                        $query = $conexion->query($sql);
+                        $max = mysqli_fetch_array($query);
+                        $sql = "UPDATE Historial_Plaza SET fecha_fin = '" . $ano . "-12-31' WHERE id_historial_plaza = ".$max[0];
+                        $conexion->query($sql);
 
-                    $sql = "UPDATE Plaza SET RFC = NULL WHERE id_plaza = " . $plaza["id_plaza"];
-                    $conexion->query($sql);
+                        $sql = "INSERT INTO Plaza(id_puesto, dias, RFC) VALUES(" . $id_puesto . ", 365, '" . $plaza["RFC"] . "')";
+                        $conexion->query($sql);
+                        $id_plaza = mysqli_insert_id($conexion);
+    
+                        $ano = date("Y");
+                        $sql = "INSERT INTO Historial_Plaza(id_plaza,fecha_inicio,RFC) VALUES(" . $id_plaza . ",'" . $ano . "-01-01','" . $plaza["RFC"] . "')";
+                        $conexion->query($sql);
+                        
+                        $total_plazas++;
+                    }else{
+                        $limite = $limite."<h5>".$plaza["RFC"] . " NO SE ENCONTRARON PLAZAS DISPONIBLES</h5><br>";
+                    }
 
-                    $ano = date("Y", strtotime($plaza["elaboracion"]));
-                    $sql = "UPDATE Historial_Plaza SET fecha_fin = '" . $ano . "-12-31'
-                    WHERE id_historial_plaza = (SELECT id_historial_plaza WHERE RFC = '" . $plaza["RFC"] . "'
-                    ORDER BY elaboracion desc LIMIT 1)";
-                    $conexion->query($sql);
-                    $id_plaza = mysqli_insert_id($conexion);
-
-                    $ano = date("Y");
-                    $sql = "INSERT INTO Historial_Plaza(id_plaza,fecha_inicio,RFC) VALUES(
-                        " . $id_plaza . ",'" . $ano . "-01-01','" . $plaza["RFC"] . "')";
-                    $conexion->query($sql);
+                    // GENERAR EXCEL
+                    // QUE VA A PASAR CON LOS REPORTES DE ANTES YA QUE LA SPLAZAS QUEDAN VACIAS
+                    // BLOQUEAR PLAZAS DE ANTES
                 }
             }
 
@@ -164,12 +176,6 @@ if ($datos[0] != "PUESTO" && $datos[1] != "DEPARTAMENTO" && $datos[2] != "DIAS P
                 }
             }
             // --------------------------------------------------------------------------------------------------
-
-            // $sql = "INSERT INTO Plaza(id_puesto, dias) VALUES(" . $id_puesto . ", " . $dias . ")";
-            // for ($i = 0; $i < $cantidad; $i++) {
-            //     $conexion->query($sql);
-            //     $total_plazas++;
-            // }
         } else if ($consulta && $total == 0) {
             $sql = "SELECT * FROM Trabajador WHERE nombre = '" . $trabajador . "'";
             $consulta = $conexion->query($sql);
@@ -201,6 +207,22 @@ if ($datos[0] != "PUESTO" && $datos[1] != "DEPARTAMENTO" && $datos[2] != "DIAS P
 
     // --------------------------------------------------------------------------------------------------
 
+    $ano = date("Y");
+    $sql = "UPDATE Plaza SET RFC = NULL WHERE RFC IS NOT NULL AND YEAR(elaboracion) <> ".$ano;
+    $conexion->query($sql);
+    $sql = "UPDATE Plaza SET estado = 0 WHERE YEAR(elaboracion) <> ".$ano;
+    $conexion->query($sql);
+
+    $sql = "SELECT * FROM Historial_Plaza WHERE fecha_fin IS NULL AND YEAR(elaboracion) <> ".$ano;
+    $query = $conexion->query($sql);
+    while($historial_plaza = mysqli_fetch_array($query)){
+        $ano = date("Y", strtotime($historial_plaza["elaboracion"])); 
+        $sql = "UPDATE Historial_Plaza SET fecha_fin = '" . $ano . "-12-31' WHERE id_historial_plaza = ".$historial_plaza["id_historial_plaza"];
+        $conexion->query($sql);
+    }
+
+    // --------------------------------------------------------------------------------------------------
+
     $highestRow--;
     echo "<div class='log'>";
     echo "<div class='log_titulo'>REGISTRO DE IMPORTACIÓN</div>";
@@ -208,6 +230,7 @@ if ($datos[0] != "PUESTO" && $datos[1] != "DEPARTAMENTO" && $datos[2] != "DIAS P
     echo "<h5> Se han agregado " . $total_plazas . " <span>plazas en total.</span></h5>";
     echo "<h5> Se encontraron " . $total_puestos . " <span> puestos nuevos.</span></h5>";
     echo "<h5> Se encontraron " . $total_departamentos . " <span> departamentos nuevos.</span></h5>";
+    echo "<div>".$limite."</div>";
 
     if (sizeof($errores) > 0) {
         echo "<h5>La siguiente lista muesta las filas no importadas. </h5>";
