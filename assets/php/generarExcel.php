3fd21b3a5f7d1a30e2c53253c6574d295c2da049
nchavezs <n.chavezsantoyo@ugto.mx>
2023-01-26 16:01:31 -0600

    fff

--- assets/php/generarExcel.php
+++ assets/php/generarExcel.php
@@ -30,7 +30,7 @@ $sql = "SELECT *,
 	(SELECT id_plaza FROM Plaza WHERE RFC = Empleado.RFC) AS plaza,
 	(SELECT nombre FROM Departamento WHERE id_departamento = (SELECT Puesto.id_departamento FROM Puesto WHERE id_puesto = Empleado.id_puesto)) AS departamento,
 	(SELECT nombre FROM Trabajador WHERE id_trabajador = (SELECT id_trabajador FROM Puesto WHERE id_puesto = Empleado.id_puesto )) AS trabajador 
-	FROM Empleado LEFT JOIN Usuario ON Usuario.RFC = Empleado.RFC WHERE 
+	FROM Empleado LEFT JOIN Usuario ON Empleado.RFC = Usuario.RFC WHERE 
     id_periodo = ".$id_periodo." AND 
     estado = '".$estado."'";
 
