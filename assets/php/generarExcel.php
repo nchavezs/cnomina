f215d77c474cd72844aa83905ac67b39fd9ca5dc
nchavezs <n.chavezsantoyo@ugto.mx>
2023-01-26 14:08:00 -0600

    xxx

--- assets/php/generarExcel.php
+++ assets/php/generarExcel.php
@@ -56,7 +56,7 @@ $sheet->setCellValue('L1', 'CUENTA BANCARIA');
 $sheet->setCellValue('M1', '# DE AFILIACIÓN');
 $sheet->setCellValue('N1', 'PERIODO');
 
-if ($consulta && (mysqli_num_rows($consulta) > 0)) {
+if ((mysqli_num_rows($consulta) > 0)) {
     $i = 2;
     while ($res = mysqli_fetch_array($consulta)) {
         $sheet->getCell('A' . $i)->setValueExplicit(str_pad($res["id_empleado"], 5, '0', STR_PAD_LEFT), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
@@ -83,8 +83,6 @@ if ($consulta && (mysqli_num_rows($consulta) > 0)) {
     foreach (range('A', $ultimo) as $columnID) {
         $sheet->getColumnDimension($columnID)->setAutoSize(true);
     }
-}else{
-    echo mysqli_error();
 }
 
 $conexion->close();
