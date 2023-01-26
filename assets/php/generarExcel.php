748a1aa1c19cbd7cbb9d6fcd0e4995fe5ce78e73
nchavezs <n.chavezsantoyo@ugto.mx>
2023-01-26 16:11:55 -0600

    FFF

--- assets/php/generarExcel.php
+++ assets/php/generarExcel.php
@@ -24,16 +24,13 @@ $contenido = [
 	],
 ];
 
-$sql = "SELECT 
-    Usuario.estado,
-    Usuario.RFC AS RFC,
-    Empleado.*,
+$sql = "SELECT *,
     (SELECT nombre FROM Periodo WHERE id_periodo = Empleado.id_periodo) AS periodo,
 	(SELECT nombre FROM Puesto WHERE id_puesto = Empleado.id_puesto) AS puesto,
-	(SELECT id_plaza FROM Plaza WHERE RFC = Empleado.RFC) AS plaza,
+	(SELECT MAX(id_plaza) FROM Plaza WHERE RFC = Empleado.RFC) AS plaza,
 	(SELECT nombre FROM Departamento WHERE id_departamento = (SELECT Puesto.id_departamento FROM Puesto WHERE id_puesto = Empleado.id_puesto)) AS departamento,
 	(SELECT nombre FROM Trabajador WHERE id_trabajador = (SELECT id_trabajador FROM Puesto WHERE id_puesto = Empleado.id_puesto )) AS trabajador 
-	FROM Usuario LEFT JOIN Empleado ON Usuario.RFC = Empleado.RFC WHERE 
+	FROM Empleado LEFT JOIN Usuario ON Usuario.RFC = Empleado.RFC WHERE 
     id_periodo = ".$id_periodo." AND 
     estado = '".$estado."'";
 
