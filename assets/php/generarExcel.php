208effcfc04e51a8b9ae75cb3a3260ec87c149f5
nchavezs <n.chavezsantoyo@ugto.mx>
2023-01-26 14:03:03 -0600

    prueba

--- assets/php/generarExcel.php
+++ assets/php/generarExcel.php
@@ -24,15 +24,16 @@ $contenido = [
 	],
 ];
 
-$sql = "SELECT *,
+$sql = "SELECT Empleado.*,
+    Usuario.estado AS estado,
     (SELECT nombre FROM Periodo WHERE id_periodo = Empleado.id_periodo) AS periodo,
 	(SELECT nombre FROM Puesto WHERE id_puesto = Empleado.id_puesto) AS puesto,
 	(SELECT id_plaza FROM Plaza WHERE RFC = Empleado.RFC) AS plaza,
 	(SELECT nombre FROM Departamento WHERE id_departamento = (SELECT Puesto.id_departamento FROM Puesto WHERE id_puesto = Empleado.id_puesto)) AS departamento,
 	(SELECT nombre FROM Trabajador WHERE id_trabajador = (SELECT id_trabajador FROM Puesto WHERE id_puesto = Empleado.id_puesto )) AS trabajador 
 	FROM Empleado LEFT JOIN Usuario ON Usuario.RFC = Empleado.RFC WHERE 
-    id_periodo = ".$id_periodo." AND 
-    estado = 'alta'";
+    Empleado.id_periodo = ".$id_periodo." AND 
+    estado = '".$estado."'";
 
 $consulta = $conexion->query($sql);
 
